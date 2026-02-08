f48ae72110e689a48e6f7e8e26b997163ec63bf8
Thalles A.S. <thalles.al.slv@gmail.com>
2026-02-08 16:41:36 -0300

    Add routes to the application

--- first-app/src/app/app.ts
+++ first-app/src/app/app.ts
@@ -1,14 +1,17 @@
 import {Component} from '@angular/core';
+import {RouterLink, RouterOutlet} from '@angular/router';
 import {Home} from "./home/home";
 
 @Component({
   selector: 'app-root',
-  imports: [Home],
+  imports: [Home, RouterLink, RouterOutlet],
   template: `
     <main>
-      <header class="brand-name"><img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true"/></header>
+      <a [routerLink]="['/']">
+        <header class="brand-name"><img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true"/></header>
+      </a>
       <section class="content">
-        <app-home/>
+        <router-outlet/>
       </section>
     </main>
   `,
