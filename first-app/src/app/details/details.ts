b80783ec13993ab3171aebcc38b76456942fe44c
Thalles A.S. <thalles.al.slv@gmail.com>
2026-02-08 21:10:09 -0300

    P.S.: correções de incômodos com formatação de código, penduricalhos e inconsistências com o código gerado durante o curso; além de adaptação da feature de busca para algo que me agrade mais.

--- first-app/src/app/details/details.ts
+++ first-app/src/app/details/details.ts
@@ -2,76 +2,44 @@ import { ChangeDetectorRef, Component, inject } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule } from "@angular/forms";
 import { ActivatedRoute } from '@angular/router';
 
+import { HousingService } from '../housing.service';
 import { HousingLocationInfo } from '../housinglocation';
-import { Housing } from '../housing';
 
 @Component({
-  selector: 'app-details',
-  imports: [ReactiveFormsModule],
-  template: `
-    <article>
-      <img
-        class="listing-photo"
-        [src]="housingLocation?.photo"
-        alt="Exterior photo of {{ housingLocation?.name }}"
-        crossorigin
-      />
-      <section class="listing-description">
-        <h2 class="listing-heading">{{ housingLocation?.name }}</h2>
-        <p class="listing-location">{{ housingLocation?.city }}, {{ housingLocation?.state }}</p>
-      </section>
-      <section class="listing-features">
-        <h2 class="section-heading">About this housing location</h2>
-        <ul>
-          <li>Units available: {{ housingLocation?.availableUnits }}</li>
-          <li>Does this location have wifi: {{ housingLocation?.wifi }}</li>
-          <li>Does this location have laundry: {{ housingLocation?.laundry }}</li>
-        </ul>
-      </section>
-      <section class="listing-apply">
-        <h2 class="section-heading">Apply now to live here</h2>
-        <form [formGroup]="applicationForm" (submit)="submitApplication()">
-          <label for="first-name">First Name</label>
-          <input id="first-name" type="text" formControlName="firstName" />
-          <label for="last-name">Last Name</label>
-          <input id="last-name" type="text" formControlName="lastName" />
-          <label for="email">Email</label>
-          <input id="email" type="email" formControlName="email" />
-          <button type="submit" class="primary">Apply now</button>
-        </form>
-      </section>
-    </article>
-  `,
-  styleUrls: ['details.css'],
+    selector: 'app-details',
+    imports: [ReactiveFormsModule],
+    templateUrl: `./details.html`,
+    styleUrls: ['details.css'],
 })
 export class Details {
-  private readonly changeDetectorRef = inject(ChangeDetectorRef);
-  route: ActivatedRoute = inject(ActivatedRoute);
-  housingLocationId = -1;
+    private readonly changeDetectorRef = inject(ChangeDetectorRef);
+    housingService = inject(HousingService);
 
-  housingService = inject(Housing);
-  housingLocation: HousingLocationInfo | undefined;
+    route: ActivatedRoute = inject(ActivatedRoute);
+    housingLocationId = -1;
 
-  applicationForm = new FormGroup({
-    firstName: new FormControl(''),
-    lastName: new FormControl(''),
-    email: new FormControl(''),
-  })
+    housingLocation: HousingLocationInfo | undefined;
 
-  constructor() {
-    this.housingLocationId = parseInt(this.route.snapshot.params['id'], 10);
-    this.housingService.getHousingLocationById(this.housingLocationId)
-        .then((housingLocation) => {
-          this.housingLocation = housingLocation;
-          this.changeDetectorRef.markForCheck();
-        });
-  }
+    applicationForm = new FormGroup({
+        firstName: new FormControl(''),
+        lastName: new FormControl(''),
+        email: new FormControl(''),
+    })
 
-  submitApplication() {
-    this.housingService.submitApplication(
-        this.applicationForm.value.firstName ?? '',
-        this.applicationForm.value.lastName ?? '',
-        this.applicationForm.value.email ?? '',
-    );
-  }
+    constructor() {
+        this.housingLocationId = parseInt(this.route.snapshot.params['id'], 10);
+        this.housingService.getHousingLocationById(this.housingLocationId)
+            .then((housingLocation) => {
+                this.housingLocation = housingLocation;
+                this.changeDetectorRef.markForCheck();
+            });
+    }
+
+    submitApplication() {
+        this.housingService.submitApplication(
+            this.applicationForm.value.firstName ?? '',
+            this.applicationForm.value.lastName ?? '',
+            this.applicationForm.value.email ?? ''
+        );
+    }
 }
--- first-app/src/app/home/home.ts
+++ first-app/src/app/home/home.ts
@@ -1,52 +1,39 @@
 import { ChangeDetectorRef, Component, inject } from '@angular/core';
+import { FormsModule } from "@angular/forms";
 
 import { HousingLocation } from "../housing-location/housing-location";
 import { HousingLocationInfo } from "../housinglocation";
-import { Housing } from "../housing"
-import {submit} from "@angular/forms/signals";
+import { HousingService } from "../housing.service"
 
 @Component({
-  selector: 'app-home',
-  imports: [HousingLocation],
-  template: `
-    <section>
-      <form>
-        <input type="text" placeholder="Filter by city" #filter/>
-        <button class="primary" type="button" (click)="filterResults(filter.value) ">Search</button>
-      </form>
-    </section>
-    <section class="results">
-      @for (housingLocation of filteredHousingLocationList; track $index) {
-        <app-housing-location [housingLocation]="housingLocation" />
-      }
-    </section>
-  `,
-  styleUrls: ['./home.css'],
+    selector: 'app-home',
+    imports: [HousingLocation, FormsModule],
+    templateUrl: `./home.html`,
+    styleUrls: ['./home.css'],
 })
 export class Home {
-  private readonly changeDetectorRef = inject(ChangeDetectorRef);
-  housingLocationList: HousingLocationInfo[] = [];
-  filteredHousingLocationList: HousingLocationInfo[] = this.housingLocationList;
-  housingService: Housing = inject(Housing);
+    private readonly changeDetectorRef = inject(ChangeDetectorRef);
+    housingService: HousingService = inject(HousingService);
 
-  constructor() {
-    this.housingService.getAllHousingLocations()
-        .then((housinglocationList: HousingLocationInfo[]) => {
-          this.housingLocationList = housinglocationList;
-          this.filteredHousingLocationList = housinglocationList;
-          this.changeDetectorRef.markForCheck();
-        });
-  }
+    housingLocationList: HousingLocationInfo[] = [];
+    filteredHousingLocationList: HousingLocationInfo[] = this.housingLocationList;
 
-  filterResults(text: string) {
-    if (!text) {
-      this.filteredHousingLocationList = this.housingLocationList;
-      return;
+    constructor() {
+        this.housingService.getAllHousingLocations()
+            .then((housinglocationList: HousingLocationInfo[]) => {
+                this.housingLocationList = housinglocationList;
+                this.filteredHousingLocationList = housinglocationList;
+                this.changeDetectorRef.markForCheck();
+            });
     }
-    this.filteredHousingLocationList = this.housingLocationList.filter((housingLocation) =>
-      housingLocation?.city.toLowerCase().includes(text.toLowerCase())
-    )
-  }
 
-  protected readonly submit = submit;
+    filterResults(text: string) {
+        if (!text) {
+            this.filteredHousingLocationList = this.housingLocationList;
+            return;
+        }
+        this.filteredHousingLocationList = this.housingLocationList.filter(
+            (housingLocation) => housingLocation?.city.toLowerCase().includes(text.toLowerCase())
+        )
+    }
 }
